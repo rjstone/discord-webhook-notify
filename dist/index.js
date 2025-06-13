53a65d5e327ea29a44b37ec707cf855a4de9338f
rjstone <rjstone@github>
2025-06-13 14:20:37 -0700

    chore: forgot to compile

--- dist/index.js
+++ dist/index.js
@@ -120380,13 +120380,6 @@ module.exports = require("console");
 
 /***/ }),
 
-/***/ 49140:
-/***/ ((module) => {
-
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("constants");
-
-/***/ }),
-
 /***/ 76982:
 /***/ ((module) => {
 
@@ -120538,6 +120531,13 @@ module.exports = require("node:http2");
 /***/ ((module) => {
 
 module.exports = require("node:net");
+
+/***/ }),
+
+/***/ 48161:
+/***/ ((module) => {
+
+module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("node:os");
 
 /***/ }),
 
@@ -124907,36 +124907,48 @@ __nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependen
 /* harmony export */   e: () => (/* binding */ run)
 /* harmony export */ });
 /* harmony import */ var discord_js__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(91508);
+/* harmony import */ var node_fs__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(73024);
+/* harmony import */ var node_os__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(48161);
+/* harmony import */ var node_path__WEBPACK_IMPORTED_MODULE_3__ = __nccwpck_require__(76760);
+/* harmony import */ var node_timers_promises__WEBPACK_IMPORTED_MODULE_4__ = __nccwpck_require__(58500);
+// A GitHub Action to notify a channel on Discord using Discord webhooks.
+
+//const core = await import('@actions/core');
+//const github = await import('@actions/github');
+
 const core = await Promise.all(/* import() */[__nccwpck_require__.e(844), __nccwpck_require__.e(367)]).then(__nccwpck_require__.t.bind(__nccwpck_require__, 37484, 19));
 const github = await Promise.all(/* import() */[__nccwpck_require__.e(844), __nccwpck_require__.e(228)]).then(__nccwpck_require__.t.bind(__nccwpck_require__, 93228, 19));
 
 
 
-const lockFile = await __nccwpck_require__.e(/* import() */ 596).then(__nccwpck_require__.t.bind(__nccwpck_require__, 43596, 19));
-const os = await Promise.resolve(/* import() */).then(__nccwpck_require__.t.bind(__nccwpck_require__, 70857, 19));
-const path = await Promise.resolve(/* import() */).then(__nccwpck_require__.t.bind(__nccwpck_require__, 16928, 19));
 
 
-const default_holddownTime = 2000; // ms
-const default_avatarUrl = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png";
-const default_username = "GitHub";
-const default_colors = {
+
+
+
+
+// Various Defaults
+const lockfileName = 'discord-webhook-lastrun-time.lock';
+const defaultHolddownTime = 3000; // ms
+const defaultAvatarUrl = "https://cdn.jsdelivr.net/gh/rjstone/discord-webhook-notify@main/img/default_avatar.png";
+const defaultUsername = "Notification (GitHub)";
+const defaultColors = {
     info: '#00ff00',
     warn: '#ff9900',
     error: '#ff0000'
 }
-const long_severity = {
+const longSeverity = {
     info: "Informational",
     warn: "Warning",
     error: "Error"
 }
 
+// This default is very minimal and its much better to create one yourself.
+// See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/accessing-contextual-information-about-workflow-runs#github-context
 async function getDefaultDescription() {
-    // This default is very minimal and its much better to construct one yourself.
-    // See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/accessing-contextual-information-about-workflow-runs#github-context
     const context = github.context;
 
-    // There are now a lot more event types than there were originally.
+    // There are now a lot more event types than there were long ago.
     // See https://docs.github.com/en/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows
     switch(context.eventName) {
     case 'push':
@@ -124962,11 +124974,51 @@ async function getDefaultDescription() {
     }
 }
 
+// Ensure some number of ms has passed since our last run, which should also be
+// the last time a Discord API request was sent. We use the modification time
+// of a "lock" file named in input lockfile_dir to determine when the last run was.
+async function ensureDurationSinceLastRun(minIntervalSinceLastRunMs) {
+    const lockfileDir = core.getInput('lockfile_dir') || '.';
+    const lockfilePath = node_path__WEBPACK_IMPORTED_MODULE_3__.join(lockfileDir, lockfileName);
+    const nowDateObj = new Date();
+
+    // TMP DEBUG
+    core.debug("ensureDuration lockfilePath: " + lockfilePath);
+
+    // If there's no file then we haven't run in "forever"
+    // so just create the file and return.
+    if (!node_fs__WEBPACK_IMPORTED_MODULE_1__.existsSync(lockfilePath)) {
+        node_fs__WEBPACK_IMPORTED_MODULE_1__.writeFileSync(lockfilePath, nowDateObj.toISOString());
+        return;
+    }
+
+    // Otherwise, figure out how long since we last ran.
+    const lockFileStat = node_fs__WEBPACK_IMPORTED_MODULE_1__.statSync(lockfilePath);
+    const nowMs = nowDateObj.getTime();
+    var sinceLastRunMs = nowMs - lockFileStat.mtimeMs;
+
+    // If we need to wait any longer to make sure the minimum time period has
+    // passed since we last ran, calculate how long we need to wait and wait
+    // slightly longer just to make sure.
+    if ( sinceLastRunMs < minIntervalSinceLastRunMs) {
+        if (sinceLastRunMs < 0) { sinceLastRunMs = 0; } // just in case
+        const waitTimeMs = minIntervalSinceLastRunMs - (sinceLastRunMs * 0.9);
+        core.debug("Waiting " + waitTimeMs + "ms");
+        return node_timers_promises__WEBPACK_IMPORTED_MODULE_4__.scheduler.wait(waitTimeMs);
+    }
+}
+
+// update the lockfile after a successful run
+async function updateLockFileTime() {
+    const lockfileDir = core.getInput('lockfile_dir') || './';
+    const lockfilePath = node_path__WEBPACK_IMPORTED_MODULE_3__.join(lockfileDir, lockfileName);
+    const nowDateObj = new Date();
+    return node_fs__WEBPACK_IMPORTED_MODULE_1__.writeFileSync(lockfilePath, nowDateObj.toISOString());
+}
+
 // For local debugging. Lack of error checking here not such a big deal.
-// Normally doesn't get executed so all these imports don't happen.
 async function getDebugTestUrl() {
-    const fs = await Promise.resolve(/* import() */).then(__nccwpck_require__.t.bind(__nccwpck_require__, 73024, 19));
-    return fs.readFileSync(path.join(os.homedir(),'github_webhookUrl.txt'),'utf8');
+    return node_fs__WEBPACK_IMPORTED_MODULE_1__.readFileSync(node_path__WEBPACK_IMPORTED_MODULE_3__.join(node_os__WEBPACK_IMPORTED_MODULE_2__.homedir(),'github_webhookUrl.txt'),'utf8');
 }
 
 async function run() {
@@ -124974,6 +125026,7 @@ async function run() {
         var webhookUrl = core.getInput('webhookUrl').replace("/github", "");
         if (webhookUrl === 'useTestURL') {
             webhookUrl = await getDebugTestUrl();
+            core.debug("Using local debug webhook in " + webhookUrl);
         } else if (!webhookUrl) {
             core.warning("The webhookUrl was not provided. For security reasons the secret URL must be provided "
                            + "in the action yaml using a context expression and can not be read as a default.");
@@ -124981,8 +125034,8 @@ async function run() {
         }
 
         // goes in message
-        const username = core.getInput('username')  || default_username;
-        const avatarUrl = core.getInput('avatarUrl')  || default_avatarUrl;
+        const username = core.getInput('username')  || defaultUsername;
+        const avatarUrl = core.getInput('avatarUrl')  || defaultAvatarUrl;
         const text = core.getInput('text') || '';
 
         // goes in embed in message
@@ -124995,15 +125048,15 @@ async function run() {
 
         const context = github.context;
         const obstr = JSON.stringify(context, undefined, 2);
-        core.debug(`The event github.context: ${obstr}`);
+        // core.debug(`The event github.context: ${obstr}`);
 
         const webhookClient = new discord_js__WEBPACK_IMPORTED_MODULE_0__.WebhookClient({url: webhookUrl}, {rest: {globalRequestsPerSecond: 10}});
 
         const embed = new discord_js__WEBPACK_IMPORTED_MODULE_0__.EmbedBuilder()
-            .setTitle(title || long_severity[severity])
-            .setColor(color || default_colors[severity])
+            .setTitle(title || longSeverity[severity])
+            .setColor(color || defaultColors[severity])
             .setDescription((description || await getDefaultDescription()) + "\n" + details)
-            .setFooter({text: footer || ("Severity: " + long_severity[severity])})
+            .setFooter({text: footer || ("Severity: " + longSeverity[severity])})
             .setTimestamp();
 
         const msg = {
@@ -125013,28 +125066,22 @@ async function run() {
             embeds: [ embed ]
         };
 
-        const holddownTime = Number.parseInt(core.getInput('holddownTime'), 10) || default_holddownTime;
-        const lockfile_name = path.join(os.tmpdir(), 'discord-webhook-notify-rate-limiting.lock');
+        core.debug("Before: " + Date());
+        const holddownTime = Number.parseInt(core.getInput('holddownTime'), 10) || defaultHolddownTime;
+        await ensureDurationSinceLastRun(holddownTime);
+        core.debug("After: " + Date());
 
-        var promise;
-        lockFile.lock(lockfile_name, { stale: holddownTime, wait: ( holddownTime * 1.5 ) }, function (err) {
-            if (err) {
-                throw new Error("Somehow failed to acquire lock file. holddownTime = ${holddownTime}");
-            } else {
-                promise = webhookClient.send(msg);
-            }
-        });
-        await promise;
-        // We intentionally don't unlock because we want any new calls to have to wait until stale.
-        // lockFile.unlock(lockfile_name, function (err) { })
+        await webhookClient.send(msg);
 
     } catch (error) {
-        // core.setFailed(error.message);
         core.notice(error.message);
+        return;
     }
+
+    await updateLockFileTime();
 }
 
-run();
+await run();
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } }, 1);
 
