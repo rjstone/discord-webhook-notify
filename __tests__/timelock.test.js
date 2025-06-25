c9ec326d35f7dfed6a294e9745eccb378ca44ed6
rjstone <rjstone@github>
2025-06-25 14:58:42 -0700

    test: finish covering timelock.js

--- __tests__/timelock.test.js
+++ __tests__/timelock.test.js
@@ -1,10 +1,15 @@
 /**
  *  Unit tests for src/timelock.js
  */
+import timersPromises from "node:timers/promises"
+import fs from "node:fs";
+import path from "node:path";
 
 import { jest } from "@jest/globals";
 
 import * as core from "../__fixtures__/actions/core.js";
+import * as defaults from "../src/defaults";
+import { getSystemErrorMap } from "node:util";
 
 // Mocks should be declared before the module being tested is imported.
 jest.unstable_mockModule('@actions/core', () => core);
@@ -13,8 +18,19 @@ jest.unstable_mockModule('@actions/core', () => core);
 // mocks are used in place of any actual dependencies.
 const timelock = await import("../src/timelock");
 
+const lockfileDir = core.getInput("lockfileDir") || ".";
+const lockfilePath = path.join(lockfileDir, defaults.lockfileName);
+
 describe("timelock.js", () => {
   beforeEach(() => {
+    try {
+      fs.unlinkSync(lockfilePath);
+    } catch (e) {
+      if ( e.code !== "ENOENT" ) {
+        throw e;
+      }
+    }
+
     // Set the action's inputs as return values from core.getInput().
     core.getInput.mockImplementation((input) => {
       return {
@@ -60,6 +76,24 @@ describe("timelock.js", () => {
 
     expect(core.info).toHaveBeenCalled();
     expect(core.info.mock.lastCall[0]).toMatch(/[Ww]aiting: \d+ms/);
+  });
+
+  it("doesn't delay when the lockfile is old enough", async () => {
+    await timelock.updateLockFileTime();
+    await timersPromises.scheduler.wait(500);
+    const start_time = Date.now();
+    await timelock.ensureDurationSinceLastRun(100);
+    expect(Date.now() - start_time).toBeLessThan(50);
+  })
+
+  it("creates lockfile and doesn't delay when there's no lockfile", async () => {
+    const start_time = Date.now();
+    // make sure lockfile was deleted
+    expect(() => { fs.statSync(lockfilePath) }).toThrow();
+    // lockfile should be created with no wait
+    await timelock.ensureDurationSinceLastRun(3000);
+    expect(() => { fs.statSync(lockfilePath) }).not.toThrow();
+    expect(Date.now() - start_time).toBeLessThan(50);
   });
 
   test.todo("Works properly if getInput('lockfile_dir') returns undefined");
